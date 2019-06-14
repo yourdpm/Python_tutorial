eda7e46f532213a7696b70f6d147b9843d601006
Hoang Thi Linh <hoanglinh831999@gmail.com>
2019-06-14 15:19:04 +0700

    Create variables.py

---
+++ Variables and Simple Data Types/variables.py
@@ -0,0 +1,16 @@
+# You can add (++, subtract (-), multiply (*), and divide (/)
+# Modulus (%), Exponent (**), Floor Division (//)
+# integers in Python
+print(2 + 3)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+
+# Python uses two multiplication symbols to represent exponents
+print(3 ** 2)
+print(3  ** 3)
+print(10 ** 6)
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
