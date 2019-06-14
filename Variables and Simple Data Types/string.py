b200cb926d5024dbc397f1f1995130813b48ea03
Hoang Thi Linh <hoanglinh831999@gmail.com>
2019-06-14 15:18:08 +0700

    Update string.py

--- Variables and Simple Data Types/string.py
+++ Variables and Simple Data Types/string.py
@@ -8,3 +8,15 @@ last_name = 'hoang'
 full_name = first_name + ' ' + last_name
 message = 'Hi ' + full_name.title() + '!'
 print(message)
+
+print('\tPython')
+print('Languages:\nPython\nC++\nJavaScript')
+
+favorite_language = ' python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+
+author = 'Albert Einstein'
+quote = 'A person who never made a mistake never tried anything new'
+print(author + ' once said, ' + '"' + quote + '."' )
