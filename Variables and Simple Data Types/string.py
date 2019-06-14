d1a0a6e0cd1e7264edc4cf0299d5f210f3ebb431
Hoang Thi Linh <hoanglinh831999@gmail.com>
2019-06-14 11:04:06 +0700

    Create string.py

---
+++ Variables and Simple Data Types/string.py
@@ -0,0 +1,10 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = 'linh'
+last_name = 'hoang'
+full_name = first_name + ' ' + last_name
+message = 'Hi ' + full_name.title() + '!'
+print(message)
